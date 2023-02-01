5fdea2c2c429f2938a20a335a35d4d46e489b1d2
pablogalindo15 <92609746+pablogalindo15@users.noreply.github.com>
2023-01-31 23:03:26 -0500

    Add files via upload

---
+++ Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/modificacion.java
@@ -0,0 +1,24 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+
+public class modificacion {
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	{
+		
+		System.out.println("Hola mundo!");
+		
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		
+		System.out.println(calc.paisConMasMedallistas());
+	}
+	
+	
+}
